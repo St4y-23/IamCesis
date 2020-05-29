6de531946cdb1994824044a1e5b947e9413ead15
St4y-23 <ghost19011@gmail.com>
2020-05-29 16:45:52 +0400

    added blog page

--- src/js/main.js
+++ src/js/main.js
@@ -129,6 +129,15 @@ $(function () {
   });
 
 
+  $('.blog__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,    
+    fade: true,
+  });
+
+
   // TABS
 
   $('.tabs a').click(function () {
@@ -218,6 +227,33 @@ $(function () {
     });
   }
 
+
+  // Magnific popup zoom effect
+  $('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+    type: 'image',
+    fixedContentPos: false,
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 
 
 
